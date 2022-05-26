37cbf1be7ed4280fa67889b6fb70bc04cb2073fe
Evan Jones <evanjones125@gmail.com>
2022-05-26 12:16:52 -0400

    create main function

--- CS-prep/largestAndSmallest.js
+++ CS-prep/largestAndSmallest.js
@@ -1,15 +1,17 @@
 const largestAndSmallest = (array) => {
-    const sortedArray = sortArr(array);
-
+    //create new array with the results of the sorting function
+    const sortedArray = sorting(array);
+    
+    //create second new array and push sortedArray at
+    //indices 0 and length - 1
     const returnArray = [];
-
     returnArray.push(sortedArray[0]);
     returnArray.push(sortedArray[sortedArray.length - 1]);
 
     return returnArray;
 }
 
-const sortArr = (nums) => nums.sort((a, b) => a - b);
+const sorting = (input) => input.sort((a, b) => a - b);
 
 console.log(largestAndSmallest([1, 2, 3, 4, 5])); // should log [1, 5]
 console.log(largestAndSmallest([90, 30, 44, 66, 10])); // should log [10, 90]
