4b35498b9dc45aab264003d8ae32f73bf4e9fe41
Evan Jones <evanjones125@gmail.com>
2022-05-27 12:34:53 -0400

    create reverseNumber function

--- CS-prep/reverseNumber.js
+++ CS-prep/reverseNumber.js
@@ -1,3 +1,11 @@
 const reverseNumber = (num) => {
-    
+    let newNum = "";
+
+    newNum += num;
+
+    return newNum.split("").reverse().join("");
+
+    return newNum;
 }
+
+console.log(reverseNumber(456));
