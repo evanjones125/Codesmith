5d1f07ee75445e1f8b7d77b8995fdcf4670f32fd
Evan Jones <evanjones125@gmail.com>
2022-05-29 19:29:54 -0400

    add pseudocode

--- CSX/pluralize.js
+++ CSX/pluralize.js
@@ -1,4 +1,10 @@
-//callback version
+// callback version
+
+// creates function pluralize, which accepts an array and a callback function;
+// pluralize will create an empty array with label output and
+// push the evaluated results of invoking the callback function at
+// arr position [i]
+
 const pluralize = (arr, instructions) => {
     const output = [];
 
@@ -14,5 +20,7 @@ const addS = word => word + 's';
 const animals = ["dog", "cat", "tree frog"];
 console.log(pluralize(animals, addS));
 
+
+// original instructions: 
 // Create a function pluralize that takes an array of strings as input and returns a new array with an "s" added to the end of each string in the input array. For example, if the string "carrot" is in the input array, it should become the string "carrots" in the output array.
 // The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.
