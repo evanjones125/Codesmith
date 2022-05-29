46130c6b516ecdb298a7ee4e05364d036b18a6ce
Evan Jones <evanjones125@gmail.com>
2022-05-29 19:21:55 -0400

    created two functions for pluralize

---
+++ CSX/pluralize.js
@@ -0,0 +1,14 @@
+//callback version
+const pluralize = (arr, instructions) => {
+    const output = [];
+
+    return output;
+}
+
+const addS = word => word + 's';
+
+const animals = ["dog", "cat", "tree frog"];
+console.log(pluralize(animals, addS));
+
+// Create a function pluralize that takes an array of strings as input and returns a new array with an "s" added to the end of each string in the input array. For example, if the string "carrot" is in the input array, it should become the string "carrots" in the output array.
+// The body of the pluralize function should employ a single for loop that pluralizes each string in the input array.
