2ad0488eb41d642db0b0062a4950dc91cea7b4eb
Evan Jones <evanjones125@gmail.com>
2022-05-30 20:54:40 -0400

    finish map function in map.js

--- CSX/map.js
+++ CSX/map.js
@@ -1,5 +1,11 @@
 const map = (arr, callback) => {
+    const output = [];
 
+    for (let i = 0; i < arr.length; i++) {
+        output.push(callback(arr[i]));
+    }
+
+    return output;
 }
 
 const subtractTwo = num => num - 2;
@@ -13,6 +19,6 @@ const subtractTwo = num => num - 2;
 // a 'callback' function - this function is applied to each element of the array (inside of the function 'map')
 // Have your map function return a new array filled with numbers that are the result of using the 'callback' function on each element of the input array. Please do not use the native map or forEach method.
 
-// map([3,4,5], subtractTwo); //-> [1,2,3]
-// subtractTwo(10); //-> 8
-// subtractTwo(12); //-> 10
+console.log(map([3,4,5], subtractTwo)); //-> [1,2,3]
+subtractTwo(10); //-> 8
+subtractTwo(12); //-> 10
