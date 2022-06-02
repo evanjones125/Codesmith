0a46a67df78a76a017b7a95db272284234a2fb0a
Evan Jones <evanjones125@gmail.com>
2022-06-02 12:29:40 -0400

    update mergeSortedArrays to solve with method chaining

--- CS-prep/PODs/mergeSortedArrays.js
+++ CS-prep/PODs/mergeSortedArrays.js
@@ -1,38 +1,40 @@
-function mergeSortedArrays(array1, array2){
+// function mergeSortedArrays(array1, array2){
 
-  //create empty array to which we will push the sorted values
-  const sorted = [];
+//   //create empty array to which we will push the sorted values
+//   const sorted = [];
 
-  if (array1.length >= array2.length) {
+//   if (array1.length >= array2.length) {
     
-    //iterate through whichever array is larger
-    //on each iteration, push arrayX[i] to sorted
-    for (let i = 0; i < array1.length; i++) {
+//     //iterate through whichever array is larger
+//     //on each iteration, push arrayX[i] to sorted
+//     for (let i = 0; i < array1.length; i++) {
 
-      //push the larger array at [i] to sorted; if arrayX[i]
-      //is smaller than the last element of sorted,
-      //splice arrayX[i] to be the second to last element
-      if (array1[i] < sorted[sorted.length - 1]) {
-        sorted.splice(sorted.length - 1, 0, array1[i]);
-      } else sorted.push(array1[i]);
+//       //push the larger array at [i] to sorted; if arrayX[i]
+//       //is smaller than the last element of sorted,
+//       //splice arrayX[i] to be the second to last element
+//       if (array1[i] < sorted[sorted.length - 1]) {
+//         sorted.splice(sorted.length - 1, 0, array1[i]);
+//       } else sorted.push(array1[i]);
 
-      //push the smaller array at [i] to sorted ONLY IF
-      //i has not exceeded the length of the smaller array
-      array2.length <= i ? null : sorted.push(array2[i]);
-    }
-  } else {
-    for (let i = 0; i < array2.length; i++) {
+//       //push the smaller array at [i] to sorted ONLY IF
+//       //i has not exceeded the length of the smaller array
+//       array2.length <= i ? null : sorted.push(array2[i]);
+//     }
+//   } else {
+//     for (let i = 0; i < array2.length; i++) {
      
-      if (array2[i] < sorted[sorted.length - 1]) {
-        sorted.splice(sorted.length - 1, 0, array2[i]);
-      } else sorted.push(array2[i]);
+//       if (array2[i] < sorted[sorted.length - 1]) {
+//         sorted.splice(sorted.length - 1, 0, array2[i]);
+//       } else sorted.push(array2[i]);
       
-      array1.length <= i ? null : sorted.push(array1[i]);
-    }
-  }
+//       array1.length <= i ? null : sorted.push(array1[i]);
+//     }
+//   }
   
-  return sorted;
-}
+//   return sorted;
+// }
+
+const mergeSortedArrays = (array1, array2) => array1.concat(array2).sort();
 
 function mergeSortedArraysTest() {
   let array1 = [1, 2, 5, 7, 9];
