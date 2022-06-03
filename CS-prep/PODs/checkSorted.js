d22907ad3dbc074ee4bf1487e48fe8c062885c61
Evan Jones <evanjones125@gmail.com>
2022-06-03 16:45:41 -0400

    create POD file and add instructions

---
+++ CS-prep/PODs/checkSorted.js
@@ -0,0 +1,5 @@
+// Given an array of integers, check to see if the array is already sorted (return true or false)
+// Focus on clearly explaining your solution
+// Once you've solved it iteratively, try to solve it recursively or using functional programming
+// If you have time, write and walk through some test cases for your code.
+// What's the time complexity of your solution?
--- CS-prep/PODs/singleNumber.js
+++ CS-prep/PODs/singleNumber.js
@@ -1,43 +1,48 @@
-function singleNumber(arr) {
-
-    //create new object to store key/value pairs of
-    //numbers with their number of occurrences
-    const obj = {};
-  
-    //iterate through obj; if the number doesn't
-    //exist in obj, add it; if it does, increment
-    arr.forEach(number => {
-      if (obj[number]) {
-        obj[number] += 1;
-      } else {
-        obj[number] = 1;
-      }
-    });
-  
-    //iterate through obj again to check which
-    //number has a value of 1; return that key
-    for (let i in obj) {
-      if (obj[i] === 1) {
-        return i;
-      }
-    }
-  
-    //contingency
-    return 'no single numbers found';
-  }
-  
-  function singleNumberTests() {
-    console.log(singleNumber([1, 2, 2]), ' -> ', 1);
-    console.log(singleNumber([1]), ' -> ', 1);
-    console.log(singleNumber([4, 1, 2, 1, 2]), ' -> ', 4);
-  }
-  
-  singleNumberTests() // uncomment to test
 
 
-//You are given an array of integers. Every number in the array appears twice, except for one. Find that single one.
-// Challenge: solve this with linear time complexity
-// Ex:
-// Input: [2, 2, 1] Output: 1
-// Input: [4, 1, 2, 1, 2] Output: 4
-// Input: [1] Output: 1
+
+console.log(twoSum([2, 5, 5, 11], 10));
+
+// function singleNumber(arr) {
+
+//     //create new object to store key/value pairs of
+//     //numbers with their number of occurrences
+//     const obj = {};
+  
+//     //iterate through obj; if the number doesn't
+//     //exist in obj, add it; if it does, increment
+//     arr.forEach(number => {
+//       if (obj[number]) {
+//         obj[number] += 1;
+//       } else {
+//         obj[number] = 1;
+//       }
+//     });
+  
+//     //iterate through obj again to check which
+//     //number has a value of 1; return that key
+//     for (let i in obj) {
+//       if (obj[i] === 1) {
+//         return i;
+//       }
+//     }
+  
+//     //contingency
+//     return 'no single numbers found';
+//   }
+  
+//   function singleNumberTests() {
+//     console.log(singleNumber([1, 2, 2]), ' -> ', 1);
+//     console.log(singleNumber([1]), ' -> ', 1);
+//     console.log(singleNumber([4, 1, 2, 1, 2]), ' -> ', 4);
+//   }
+  
+//   singleNumberTests() // uncomment to test
+
+
+// //You are given an array of integers. Every number in the array appears twice, except for one. Find that single one.
+// // Challenge: solve this with linear time complexity
+// // Ex:
+// // Input: [2, 2, 1] Output: 1
+// // Input: [4, 1, 2, 1, 2] Output: 4
+// // Input: [1] Output: 1
