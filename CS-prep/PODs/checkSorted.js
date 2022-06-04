8f65311395813ff24838e151dd1e9b11a15cb7f5
Evan Jones <evanjones125@gmail.com>
2022-06-04 13:32:55 -0400

    solve checkSorted

--- CS-prep/PODs/checkSorted.js
+++ CS-prep/PODs/checkSorted.js
@@ -1,3 +1,9 @@
+const check = arr => {
+    if (!arr[0]) return true;
+    if (arr[0] > arr[1]) return false;
+    return check(arr.slice(1));
+}
+
 // Given an array of integers, check to see if the array is already sorted (return true or false)
 // Focus on clearly explaining your solution
 // Once you've solved it iteratively, try to solve it recursively or using functional programming
