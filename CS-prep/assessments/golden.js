97f682af3e6debf6ab5ff1d63e00b05513cfbffc
Evan Jones <evanjones125@gmail.com>
2022-06-07 22:06:33 -0400

    add pseudocode to golden and roulette

--- CS-prep/assessments/golden.js
+++ CS-prep/assessments/golden.js
@@ -1,16 +1,19 @@
 const goldenSequence = n => {
+    // find nth Fibonacci term using Binet's formula
     let rootFive = Math.sqrt(5);
-    return binet = Math.round((Math.pow(((1 + rootFive) / 2), n)) / rootFive);
+    const binet = Math.round((Math.pow(((1 + rootFive) / 2), n)) / rootFive);
     
     // set base cases    
     if (n === 0) return 0;
     if (n === 1) return 1;
-    // if (n < 70) return binet;
+    // the time complexity difference between the recusive/Binet solutions ...
+    // ... caused a large disparity in processing time in favor of the Binet method
+    // however, Binet's fomula was only accurate up to n = 70 in my testing
+    if (n < 70) return binet;
 
-    // recursively find nth term in FS using the recursive formula
+    // recursively find nth Fibonacci term using the recursive formula if n >= 70
     return goldenSequence(n - 1) + goldenSequence(n - 2);
 
-    
 }
 
 console.log(goldenSequence(0)); // should log: 0
--- CS-prep/assessments/roulette.js
+++ CS-prep/assessments/roulette.js
@@ -1,6 +1,8 @@
 const roulette = n => {
+    // keep track of number of "spins"
     let spins = 1;
     
+    // return a function that checks conditions and outputs based on spins' value
     return function() {
         for (let i = 0; i < n; i++) {
             if (spins > n) return 'pick a number to play again';
