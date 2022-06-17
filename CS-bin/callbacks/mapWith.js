a684d25aa13b41389ada7e4a5ab26d06b7c58926
Evan Jones <evanjones125@gmail.com>
2022-06-17 14:00:12 -0400

    solve mapWith

---
+++ CS-bin/callbacks/mapWith.js
@@ -0,0 +1,19 @@
+const mapWith = (array, callback) => {
+    const newArray = [];
+
+    forEach(array, function(element) {
+        newArray.push(callback(element));
+    })
+
+    return newArray;
+}
+
+// function forEach(array, callback) {
+// 	for (let i = 0; i < array.length; i++) {
+//     callback(array[i]);
+//   }
+// }
+
+// const addTwo = num => num + 2;
+
+// console.log(mapWith([1, 2, 3], addTwo));
