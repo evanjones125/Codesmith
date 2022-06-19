55d8e92f0248072ca2ecd84bb6e4878afe7710a3
Evan Jones <evanjones125@gmail.com>
2022-06-19 18:33:34 -0400

    add functions to shortChallenges

--- CSX/functions and execution context/shortChallenges.js
+++ CSX/functions and execution context/shortChallenges.js
@@ -2,6 +2,18 @@
 these challenges weren't lengthy or difficult enough to warrant a full file dedicated to them
 */
 
+// ---addTwo---
+const addTwo = (num) => num += 2;
+// console.log(typeof addTwo); // should log: 'function'
+// console.log(addTwo(10)); // should log: 12
+
+
+// ---addS---
+const addS = (str) => str + 's';
+// console.log(typeof addS); // should log: 'function'
+// console.log(addS('cat')); // should log: 'cats'
+
+
 // ---addWaldo---
 const addWaldo = obj => {
     obj["Waldo"] = 'unknown';
@@ -14,3 +26,8 @@ const addWaldo = obj => {
 // ---function expression---
 const myFunc = () => "Hi there!";
 // console.log(myFunc());
+
+
+// ---ES6---
+const myJob = (name, profession) => `Hi, my name is ${name} and I'm a ${profession}.`;
+// console.log(myJob("Jon", "Knights Watchman"));
