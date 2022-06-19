28d14289a071ce7e1a19060bf8e463ff921766b8
Evan Jones <evanjones125@gmail.com>
2022-06-19 18:30:27 -0400

    add function expression to shortChallenges

--- CSX/functions and execution context/shortChallenges.js
+++ CSX/functions and execution context/shortChallenges.js
@@ -2,11 +2,15 @@
 these challenges weren't lengthy or difficult enough to warrant a full file dedicated to them
 */
 
-// addWaldo
+// ---addWaldo---
 const addWaldo = obj => {
     obj["Waldo"] = 'unknown';
     return obj;
 }
-  
-const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
-console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
+// const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+// console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
+
+
+// ---function expression---
+const myFunc = () => "Hi there!";
+// console.log(myFunc());
