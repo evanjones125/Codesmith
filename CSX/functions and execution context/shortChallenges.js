2cf5540c041879dc3f49f18e01baf52dff5fff9a
Evan Jones <evanjones125@gmail.com>
2022-06-19 18:25:52 -0400

    add addWaldo to shortChallenges and add description

---
+++ CSX/functions and execution context/shortChallenges.js
@@ -0,0 +1,12 @@
+/* 
+these challenges weren't lengthy or difficult enough to warrant a full file dedicated to them
+*/
+
+// addWaldo
+const addWaldo = obj => {
+    obj["Waldo"] = 'unknown';
+    return obj;
+}
+  
+const siliconValley = {'Richard': 'Hendricks', 'Erlich': 'Bachman', 'Bertram': 'Gilfoyle'}
+console.log(addWaldo(siliconValley)) // should log:{ Richard: 'Hendricks', Erlich: 'Bachman', Bertram: 'Gilfoyle', Waldo: 'unknown' }
