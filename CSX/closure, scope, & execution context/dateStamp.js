e1e3780f25e834ddce01b61c0b657e82728555e0
Evan Jones <evanjones125@gmail.com>
2022-06-22 18:57:01 -0400

    solve dateStamp

---
+++ CSX/closure, scope, & execution context/dateStamp.js
@@ -0,0 +1,15 @@
+const dateStamp = func => {
+    const newObj = {
+      date: Date(),
+      output: undefined
+    };
+    
+    return function(...args) {
+      for (let i in args) newObj.output = func(args[i]);
+      return newObj;
+    }
+  }
+  
+  const stampedMultBy2 = dateStamp(n => n * 2);
+  console.log(stampedMultBy2(4)); // should log: { date: (today's date), output: 8 }
+  console.log(stampedMultBy2(6)); // should log: { date: (today's date), output: 12 }
