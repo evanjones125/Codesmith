6dfdd8aeaa82bda8b6d8b699ffaa34a458c7d03e
Evan Jones <evanjones125@gmail.com>
2022-07-06 16:01:31 -0400

    solve saveOutput

---
+++ CS-bin/closure/saveOutput.js
@@ -0,0 +1,18 @@
+const saveOutput = (func, str) => {
+    const output = {};
+    
+    return function(input) {
+        if (input !== str) {
+            output[input] = func(input);
+            return func(input);
+        }
+        
+        if (input === str) return output;
+    }
+}
+
+const multiplyBy2 = function(num) { return num * 2; };
+const multBy2AndLog = saveOutput(multiplyBy2, 'boo');
+console.log(multBy2AndLog(2)); // => should log 4
+console.log(multBy2AndLog(9)); // => should log 18
+console.log(multBy2AndLog('boo')); // => should log { 2: 4, 9: 18 }
