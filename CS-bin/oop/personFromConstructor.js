3f918bfdd95fd80d443315bdaf8a44fcf8f90e65
Evan Jones <evanjones125@gmail.com>
2022-07-11 22:00:33 -0400

    solve personFromConstructor

--- CS-bin/oop/personFromConstructor.js
+++ CS-bin/oop/personFromConstructor.js
@@ -1,5 +1,14 @@
 const personFromConstructor = (name, age) => {
+    const person = new PersonConstructor();
+    person.name = name;
+    person.age = age;
+    return person;
+}
 
+function PersonConstructor() {
+    this.greet = function() {
+        console.log('hello');
+    }
 }
 
 const mike = personFromConstructor('Mike', 30);
