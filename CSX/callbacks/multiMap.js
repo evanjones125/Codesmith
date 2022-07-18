ea862bf2b0a7add52641e152e7cfeb5a242c789f
Evan Jones <evanjones125@gmail.com>
2022-07-18 10:55:34 -0400

    update multiMap to use map method

--- CSX/callbacks/multiMap.js
+++ CSX/callbacks/multiMap.js
@@ -1,12 +1,8 @@
 const multiMap = (arr1, arr2) => {
 	const newObj = {};
   
-  for (let i = 0; i < arr1.length; i++) {
-    newObj[arr1[i]] = [];
-    
-    for (let j = 0; j < arr2.length; j++) {
-      newObj[arr1[i]].push(arr2[j](arr1[i]));
-    }
+  for (let i in arr1) {
+    newObj[arr1[i]] = arr1.map(function(el, j) { return arr2[j](arr1[i]) });
   }
   
   return newObj;
